758385e799c2d934b3ecbf0cdf9e704c85a4fa21
Scate <e@inscate.com>
2024-05-24 20:30:40 +0300

    macos build

--- include/hw.h
+++ include/hw.h
@@ -7,7 +7,7 @@ struct HW
     const int display_h = 32;
     const bool original_shift = false;
     const int memory_size = 4096;
-    const int render_scaler = 24;
+    const int render_scaler = 16;
     uint8_t framebuffer[64 * 32] = {0};
     uint8_t framebuffer_rendered[64 * 32] = {0};
     uint8_t key_press[16] = {0};
